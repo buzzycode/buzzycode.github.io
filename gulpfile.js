78feb509984292997a6b775e92338f3e052672c9
Hitesh Kumar <hk.skit@hotmail.com>
2016-12-08 23:23:46 +0530

    Added default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
 'use strict';
 var gulp = require('gulp');
 var browserify = require('browserify');
-var deploy = require('gulp-gh-pages');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+var del = require('del');
+var sourcemaps = require('gulp-sourcemaps');
+var bytediff= require('gulp-bytediff');
+var size = require('gulp-size');
 
-gulp.task('deploy', function(){
-  return gulp.src('scripts/**/*.js')
-      .pipe(deploy());
+function bundlejs(){
+  var b = browserify({
+    entries: 'scripts/index.js',
+    debug: true
+  })
+  .bundle()
+  .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+          .pipe(bytediff.start())
+          .pipe(uglify())
+          .pipe(bytediff.stop())
+      .pipe(sourcemaps.write('.'))
+      .pipe(size({
+          showFiles: true
+      }))
+      .pipe(gulp.dest('dist')); 
+}
+
+gulp.task('cleanup', function(){
+  del('dist');
 });
+
+gulp.task('default', ['cleanup'],  bundlejs);
